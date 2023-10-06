e0ae55a408dc334237abce1f43fc6f1979925fca
invalid04 <68178371+invalid04@users.noreply.github.com>
2023-10-06 02:55:31 -0500

    Edited the layout file

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Navbar from "@/components/navbar/navbar"
 export default function Home() {
   return (
     <div className='bg-[#F5F5F5]'>
-      <Navbar />
       <Contact />
     </div>
   )
